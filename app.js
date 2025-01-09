b55421db98c76909b7adee3566e7ad5c265ae8ec
unknown <230693@utxicotepec.edu.mx>
2025-01-09 09:34:37 -0600

    Practica03
    Se agrego la ruta para mostrar la información de la sesión y para cerrar sesión

--- app.js
+++ app.js
@@ -21,6 +21,42 @@ app.use((req, res, next)=>{
     }
     next();
     
-})
+});
+// Ruta para mostrar la información de la sesión
+app.get('/session', (req, res)=>{
+    if(req.session) {
+        const sessionId = req.session.id;
+        const createdAt = req.session.createdAt;
+        const lastAccess = req.session.lastAccess;
+        const sessionDuration = (new Date() - createdAt) / 1000; // Duración de la sesión en segundos
+        
+    res.send(`
+    <h1>Detalles de la sesión</h1>
+    <p><strong>ID sesión:</strong> ${sessionId}</p>
+    <p><strong>Fecha de creación de la sesión:</strong>${createdAt}</p>
+    <p><strong>Último acceso: </strong> ${lastAccess}</p>
+    <p><strong>Duración de la sesión (en segundos):</strong>${sessionDuration}</p>
+    `);
+    } else{
+        res.send(`<h1>Sesión cerrada exitosamente.</h1>`)
+    }
+});
+
+// Ruta para cerrar la sesión 
+app.get('/logout', (req, res)=>{
+    req.session.destroy((err)=>{
+        if(err){
+        return res.send(`Error al cerrar la sesión.`);
+        }
+        res.send(`<h1>Sesión cerrada exitosamente</h1>`)
+    });
+
+
+});
+
+//Iniciar el servidor en el puerto 3000
+app.listen(3000,()=>{
+    console.log(`Servidor corriendo en el puerto 3000`);
+});
 
 
